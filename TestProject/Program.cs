f474b136f8e507eef4991982a18a257a26e1347c
Eric Prather <pratherea@gmail.com>
2017-02-12 18:47:29 -0800

    I completed the framework of a geometry system. It's a work in progress, as I still haven't done circles, rectangles, or shapes, but I'm most of the way finished with a comprehensive line system. This is one of the types of things which has proven very helpful at our competition in the past. A lot of this still needs to be tested, and some more of it is placeholder until I find a more readable way to do things.

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SouthSaxon;
+using SouthSaxon.Geometry;
 
 namespace TestProgram
 {
@@ -7,10 +8,15 @@ namespace TestProgram
     {
         static void Main(String[] args)
         {
-            Console.WriteLine("This is a test.");
+            /*Console.WriteLine("This is a test.");
             string target = IO.ReadString("Input a string to be searched: ");
             string pattern = IO.ReadString("Input the search pattern: ");
             Console.WriteLine("Found: " +  IO.ValidateInputItem(target, pattern));
+            Console.ReadLine();*/
+
+            Line line1 = new Line(1, 0);
+            Line line2 = new Line(-1, 0);
+            Console.WriteLine(line1.Intersect(line2));
             Console.ReadLine();
 
             //This comment is an example change to test commits: Everything's working!
